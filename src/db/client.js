4dfe508447ef534f5252533ddd96f780de91c9ad
v.lapushkin <lapushkinvitaliy@gmail.com>
2019-11-21 22:05:13 +0300

    db connection data from environment variables

--- src/db/client.js
+++ src/db/client.js
@@ -1,7 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 
 //const url = 'mongodb+srv://mongo:mongo@busylist-eifyi.mongodb.net/test?retryWrites=true&w=majority';
-const url = 'mongodb://mongo:mongo@localhost:27017';
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = process.env.DB_PORT || '27017';
+const dbLogin = process.env.DB_LOGIN || 'mongo';
+const dbPassword = process.env.DB_PASSWORD || 'mongo';
+const url = `mongodb://${dbLogin}:${dbPassword}@${dbHost}:${dbPort}`;
 const dbName = 'BusyList';
 const dbCollectionName = 'items';
 
